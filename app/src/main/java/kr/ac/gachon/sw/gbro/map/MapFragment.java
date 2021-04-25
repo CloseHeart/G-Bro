db96826027dc43618e3bf4c6c30d022f657066af
Minjae Seon <ddol0225@gmail.com>
2021-04-25 15:21:37 +0900

    Added Fab Button / Redesign Swiper

--- app/src/main/java/kr/ac/gachon/sw/gbro/map/MapFragment.java
+++ app/src/main/java/kr/ac/gachon/sw/gbro/map/MapFragment.java
@@ -1,6 +1,7 @@
 package kr.ac.gachon.sw.gbro.map;
 
 import android.os.Bundle;
+import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -14,6 +15,7 @@ import com.naver.maps.map.CameraPosition;
 import com.naver.maps.map.NaverMap;
 import com.naver.maps.map.NaverMapOptions;
 import com.naver.maps.map.OnMapReadyCallback;
+import com.naver.maps.map.widget.LocationButtonView;
 
 import kr.ac.gachon.sw.gbro.base.BaseFragment;
 import kr.ac.gachon.sw.gbro.databinding.FragmentMapBinding;
@@ -39,9 +41,19 @@ public class MapFragment extends BaseFragment<FragmentMapBinding> implements OnM
         if(mapFragment == null) {
             NaverMapOptions options = new NaverMapOptions()
                     .camera(new CameraPosition(new LatLng(37.45082183610419, 127.12877229523757), 16))
-                    .locationButtonEnabled(true);
+                    .locationButtonEnabled(true)
+                    .logoGravity(Gravity.START | Gravity.TOP)
+                    .logoMargin(8, 8, 8, 8)
+                    .locationButtonEnabled(false)
+                    .compassEnabled(false)
+                    .zoomControlEnabled(false);
             mapFragment = com.naver.maps.map.MapFragment.newInstance(options);
             fragmentManager.beginTransaction().add(binding.map.getId(), mapFragment).commit();
+
+            mapFragment.getMapAsync(naverMap -> {
+                LocationButtonView locationButtonView = binding.mapwidgetLocation;
+                locationButtonView.setMap(naverMap);
+            });
         }
     }
 
