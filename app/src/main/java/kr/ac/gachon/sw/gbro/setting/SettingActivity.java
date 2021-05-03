d8152f303fc9ad9125f6b209b5ce01f97862880a
Minjae Seon <ddol0225@gmail.com>
2021-05-03 19:05:26 +0900

    Changed AndroidX Preference / Removed Unused Code

--- app/src/main/java/kr/ac/gachon/sw/gbro/setting/SettingActivity.java
+++ app/src/main/java/kr/ac/gachon/sw/gbro/setting/SettingActivity.java
@@ -6,12 +6,19 @@ import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import kr.ac.gachon.sw.gbro.R;
+import kr.ac.gachon.sw.gbro.base.BaseActivity;
+import kr.ac.gachon.sw.gbro.databinding.ActivityChattingBinding;
+import kr.ac.gachon.sw.gbro.databinding.ActivitySettingBinding;
 
-public class SettingActivity extends AppCompatActivity {
+public class SettingActivity extends BaseActivity<ActivitySettingBinding> {
+
+    @Override
+    protected ActivitySettingBinding getBinding() {
+        return ActivitySettingBinding.inflate(getLayoutInflater());
+    }
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_setting);
     }
 }
