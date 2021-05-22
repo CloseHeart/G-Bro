421b8aed842eb82cdc1fe5c3c2798843814fb473
Minjae Seon <ddol0225@gmail.com>
2021-05-22 19:14:53 +0900

    Prepared for Keyword Notification

--- app/src/main/java/kr/ac/gachon/sw/gbro/fcm/FCMApi.java
+++ app/src/main/java/kr/ac/gachon/sw/gbro/fcm/FCMApi.java
@@ -3,6 +3,7 @@ package kr.ac.gachon.sw.gbro.fcm;
 import com.squareup.okhttp.ResponseBody;
 
 import kr.ac.gachon.sw.gbro.util.model.ChatFCMModel;
+import kr.ac.gachon.sw.gbro.util.model.KeywordFCMModel;
 import retrofit2.Call;
 import retrofit2.http.Body;
 import retrofit2.http.POST;
@@ -10,4 +11,7 @@ import retrofit2.http.POST;
 public interface FCMApi {
     @POST("fcm/send")
     Call<ResponseBody> sendChatNotification(@Body ChatFCMModel model);
+
+    @POST("fcm/send")
+    Call<ResponseBody> sendKeywordNotificaiton(@Body KeywordFCMModel model);
 }
