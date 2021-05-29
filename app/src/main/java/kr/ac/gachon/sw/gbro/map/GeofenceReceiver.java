a7decade7c6cc8657e6de1800241b0cbbf3a60e0
ucans <zyzy1231@naver.com>
2021-05-30 00:26:21 +0900

    Implement Geofencing

--- app/src/main/java/kr/ac/gachon/sw/gbro/map/GeofenceReceiver.java
+++ app/src/main/java/kr/ac/gachon/sw/gbro/map/GeofenceReceiver.java
@@ -1,70 +1,82 @@
 package kr.ac.gachon.sw.gbro.map;
 
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
-import android.os.IBinder;
+import android.content.res.Resources;
+import android.media.RingtoneManager;
+import android.os.Build;
 import android.util.Log;
-import android.widget.Toast;
 
 import androidx.annotation.NonNull;
+import androidx.core.app.NotificationCompat;
 
 import com.google.android.gms.location.Geofence;
 import com.google.android.gms.location.GeofenceStatusCodes;
 import com.google.android.gms.location.GeofencingEvent;
-import com.google.android.gms.location.GeofencingRequest;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
-import com.google.firebase.firestore.DocumentSnapshot;
 import com.google.firebase.firestore.QuerySnapshot;
 
-import java.util.ArrayList;
 import java.util.List;
 
+import kr.ac.gachon.sw.gbro.MainActivity;
+import kr.ac.gachon.sw.gbro.R;
+import kr.ac.gachon.sw.gbro.service.LocalNotiService;
 import kr.ac.gachon.sw.gbro.util.Firestore;
+import kr.ac.gachon.sw.gbro.util.Util;
 
 public class GeofenceReceiver extends BroadcastReceiver {
 
     private Context context;
-    private static String TAG = "GeofenceReceiver";
 
     @Override
     public void onReceive(Context context, Intent intent) {
+        this.context = context;
         GeofencingEvent geofencingEvent = GeofencingEvent.fromIntent(intent);
         if(geofencingEvent.hasError()){
             String errorMessage = GeofenceStatusCodes
                     .getStatusCodeString(geofencingEvent.getErrorCode());
-            Log.d(TAG, errorMessage);
+            Log.w(GeofenceReceiver.this.getClass().getSimpleName(), errorMessage);
             return;
         }
 
+        Log.d(GeofenceReceiver.this.getClass().getSimpleName(), "Geofence No Error");
         int geofenceTransition = geofencingEvent.getGeofenceTransition();
+        Log.d(GeofenceReceiver.this.getClass().getSimpleName(), "geofenceTransition : " + geofenceTransition);
 
-        // 건물 내에 내에 1분 이내로 머문 경우
-        if(geofenceTransition == Geofence.GEOFENCE_TRANSITION_DWELL){
+        if(geofenceTransition == Geofence.GEOFENCE_TRANSITION_ENTER){
             List<Geofence> triggeringGeofences = geofencingEvent.getTriggeringGeofences();
 
-            // geofencingEvent.getTriggeringLocation();
+            Log.d(GeofenceReceiver.this.getClass().getSimpleName(), "geofenceTransition = " + geofenceTransition);
+            Log.d(GeofenceReceiver.this.getClass().getSimpleName(), "triggeringGeofences : " + triggeringGeofences);
 
-            // TODO :여러 건물의 반경 안에 있는 경우, 알림 여러번?  or 한 번에 다 보여줌?
-            // TODO : NOTIFICATION("건물 이름"에 "분실물 갯수"개의 분실물이 있습니다.)
             for(Geofence currentGeofence : triggeringGeofences){
-                int postCnt = getPostCount(Integer.parseInt(currentGeofence.getRequestId()));
+                int buildingType = Integer.parseInt(currentGeofence.getRequestId());
+                int postCnt = getPostCount(buildingType);
+
                 if(postCnt == 0){
-                    Log.d(getClass().getName(), "No Post in " + currentGeofence.getRequestId());
+                    Log.d(GeofenceReceiver.this.getClass().getSimpleName(), "No Post in " + buildingType);
                 }
                 else{
-                    Log.d(getClass().getName(), "Post : " + postCnt + " in + " + currentGeofence.getRequestId());
+                    String buildingName = Resources.getSystem().getStringArray(R.array.gachon_globalcampus_building2)[buildingType];
+                    Log.d(GeofenceReceiver.this.getClass().getSimpleName(), "Post : " + postCnt + " in + " + buildingType);
+
+                    showNearbyNotification(buildingName, postCnt);
                 }
-
             }
-            // TODO : 현재 건물에 분실물 몇 개인지 알려줘야함
         }
-        else{
 
-        }
     }
 
+    /**
+     * 해당 건물에 등록된 모든 post 갯수
+     * @param buildingType int
+     * @return count
+     */
     private int getPostCount(int buildingType){
         final int[] count = {0};
         Firestore.getBuildingPost(buildingType).addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
@@ -82,4 +94,26 @@ public class GeofenceReceiver extends BroadcastReceiver {
         return count[0];
     }
 
+    private void showNearbyNotification(String buildingName, int postCnt){
+        if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.O){
+            Intent mainIntent = new Intent(context.getApplicationContext(), MainActivity.class)
+                    .addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+
+            PendingIntent pendingIntent = PendingIntent.getActivity(context.getApplicationContext(), 0, mainIntent, 0);
+
+            NotificationChannel notiChannel = new NotificationChannel(Util.NEARBY_ID, context.getResources().getString(R.string.nearbynoti_name), NotificationManager.IMPORTANCE_HIGH);
+            NotificationCompat.Builder notiBuilder = new NotificationCompat.Builder(context, Util.NEARBY_ID)
+                    .setStyle(new NotificationCompat.BigTextStyle())
+                    .setSmallIcon(R.mipmap.ic_launcher_foreground)
+                    .setContentTitle(context.getResources().getString(R.string.nearbynoti_title))
+                    .setContentText(context.getResources().getString(R.string.nearbynoti_msg, buildingName, postCnt))
+                    .setSound(RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION))
+                    .setAutoCancel(true)
+                    .setContentIntent(pendingIntent);
+
+            NotificationManager notificationManager = (NotificationManager) context.getSystemService(Context.NOTIFICATION_SERVICE);
+            notificationManager.createNotificationChannel(notiChannel);
+            notificationManager.notify(101, notiBuilder.build());
+        }
+    }
 }
